29203ce85cd19a9ea2e2500ad8e5e07f8efdfffd
Natalie Mohr <npatter2@gmail.com>
2024-06-13 13:50:05 -0400

    Fix directory structure and file placement

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
- from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -12,3 +12,4 @@ def chatgpt():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
